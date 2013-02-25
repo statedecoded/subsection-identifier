478bc51dadd6984f44b57da36474d091882e08e2
Waldo Jaquith <waldo@jaquith.org>
2013-02-25 11:24:12 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,138 @@
-subsection-identifier
-=====================
+# Subsection Identifier
 
-Turns theoretically structured text into actually structured text.
+Turns theoretically structured text into actually structured text. If you have a well-structured textual document, but with structure that exists only within the text and not actually as metadata, then this is the tool to solve that problem.
+
+
+# Instructions
+
+Take the text and break it up into paragraphs, storing the text as an object, with each paragraph as a numbered property. Create a new instance of `Subsection_Identifier` (`$parser = new SubsectionIdentifier()`), store the text object as the `text` property, and then invoke the method `parse()`. That will create a member object property, `structured`, that uses the same numbered property list as the input array, with each property’s `prefix_hierarchy`, `prefix`, and `text` stored as a property.
+
+When a structural identifer cannot be identified at the beginning of a paragraph, it will be assumed that the paragraph is continuation of the subsection identified for the prior paragraph.
+
+This method looks for structural identifiers of the following formats:
+
+* `A. `: One or two capital letters, followed by a period and a space.
+* `1. `: One or two digits, followed by a period and a space.
+* `a. `: One or two lowercase letters, followed by a period and a space.
+* `(1) ` One or two digits, wrapped in parentheses, followed by a space.
+* `(a) ` One or two lowercase letters, wrapped in parentheses, followed by a space.
+
+To add other structural identifiers, simply write a PCRE to identify them and add them to the `$prefix_candidates` array. Note that only the first 5 characters of each line are searched for a structural identifier, so if you want to find longer structural identifiers, modify `$section_fragment = substr($paragraph, 0, 5);` to extract a longer substring.
+
+
+# Example
+
+For example, this text, [from the Virginia Administrative Code](http://leg1.state.va.us/cgi-bin/legp504.exe?000+reg+16VAC20-11-80):
+
+> A. The agency may appoint a negotiated rulemaking panel (NRP) if a regulatory action is expected to be controversial.
+> B. An NRP that has been appointed by the agency may be dissolved by the agency when:
+> 1. There is no longer controversy associated with the development of the regulation;
+> 2. The agency determines that the regulatory action is either exempt or excluded from the requirements of the Administrative Process Act; or
+> 3. The agency determines that resolution of a controversy is unlikely.
+
+It is well structured, in the sense that its structure is clear to a human looking at it, but there is no metadata providing structure that is machine readable.
+
+To improve this text with this tool, first break it up into its component paragraphs and store it as an object (`$text = (object) explode(PHP_EOL, $text);`), which yields the following:
+
+```php
+stdClass Object
+(
+    [0] => A. The agency may appoint a negotiated rulemaking panel (NRP) if a regulatory action is expected to be controversial.
+    [1] => B. An NRP that has been appointed by the agency may be dissolved by the agency when:
+    [2] => 1. There is no longer controversy associated with the development of the regulation;
+    [3] => 2. The agency determines that the regulatory action is either exempt or excluded from the requirements of the Administrative Process Act; or
+    [4] => 3. The agency determines that resolution of a controversy is unlikely.
+)
+```
+
+Then feed that text into Subsection Identifier:
+
+```php
+$parser = new SubsectionIdentifier();
+$parser->text = $text;
+$parser->parse();
+```
+
+This returns the following as `$parser->structured`:
+
+```php
+(
+	[text] => 
+	[0] => stdClass Object
+		(
+			[prefix_hierarchy] => stdClass Object
+				(
+					[0] => A
+				)
+
+			[prefix] => A.
+			[text] => The agency may appoint a negotiated rulemaking panel (NRP) if a regulatory action is expected to be controversial.
+		)
+
+	[1] => stdClass Object
+		(
+			[prefix_hierarchy] => stdClass Object
+				(
+					[0] => B
+				)
+
+			[prefix] => B.
+			[text] => An NRP that has been appointed by the agency may be dissolved by the agency when:
+		)
+
+	[2] => stdClass Object
+		(
+			[prefix_hierarchy] => stdClass Object
+				(
+					[0] => B
+					[1] => 1
+				)
+
+			[prefix] => B.1.
+			[text] => There is no longer controversy associated with the development of the regulation;
+		)
+
+	[3] => stdClass Object
+		(
+			[prefix_hierarchy] => stdClass Object
+				(
+					[0] => B
+					[1] => 2
+				)
+
+			[prefix] => B.2.
+			[text] => The agency determines that the regulatory action is either exempt or excluded from the requirements of the Administrative Process Act; or
+		)
+
+	[4] => stdClass Object
+		(
+			[prefix_hierarchy] => stdClass Object
+				(
+					[0] => B
+					[1] => 3
+				)
+
+			[prefix] => B.3.
+			[text] => The agency determines that resolution of a controversy is unlikely.
+		)
+
+)
+```
+
+# Shortcomings
+
+This cannot determine when a child subsection terminates and the following paragraph belongs to the parent subsection. For example:
+
+```
+B. The following transactions are exempted from the securities, broker-dealer and agent registration requirements of this chapter: 
+
+  1. Any isolated transaction by the owner or pledgee of a security, whether effected through a broker-dealer or not, which is not directly or indirectly for the benefit of the issuer; 
+
+  2. Any nonissuer distribution by a registered broker-dealer and its registered agent of a security that has been outstanding in the hands of the public for the past five years;
+
+  If the issuer in each of the past three fiscal years has lawfully paid dividends on its common stock aggregating at least four percent of its current market price;
+
+except as expressly provided in this subsection.
+```
+
+The final fragment—“except as expressly provided in this subsection” properly belongs to B, but this method will assign it to B2. That's because unlabelled paragraphs are presumed to be a continuation of the prior paragraph; there is no method by which “except as expressly provided in this subsection” can be known to be a continuation of “and agent registration requirements of this chapter.”
---
+++ subsection-identifier.php
@@ -0,0 +1,224 @@
+<?php
+
+/**
+ * Subsection Identifier
+ *
+ * 
+ * Requires the object $this->text, with each section stored as a child ($this->text->0,
+ * $this->text->1, etc.) Returns a hierarchically structured, labelled object, named
+ * $this->structured, with a prefix_hiearchy object (containing an entry for the structural
+ * identifier of each generation in the structural ancestry, in order), a prefix entry (containing
+ * the section's structural identifer), and a text entry, with the structural identifier stripped
+ * off.
+ * 
+ * PHP version 5
+ *
+ * @author		Waldo Jaquith <waldo at jaquith.org>
+ * @copyright	2013 Waldo Jaquith
+ * @license		http://www.gnu.org/licenses/gpl.html GPL 3
+ * @version		1.0
+ * @link		http://www.statedecoded.com/
+ * @since		1.0
+ *
+ */
+
+class SubsectionIdentifier
+{
+
+	function parse()
+	{
+		
+		if (!isset($this->text))
+		{
+			return false;
+		}
+		
+		/*
+		 * Define all possible section prefixes via via PCRE strings.
+		 */
+		$prefix_candidates = array	('/[A-Z]{1,2}\. /',
+									'/[0-9]{1,2}\. /',
+									'/[a-z]{1,2}\. /',
+									'/\([0-9]{1,2}\) /',
+									'/\([a-z]{1,2}\) /');
+		
+		/*
+		 * Establish a blank prefix structure. We'll build this up and continually modify it to keep
+		 * track of our current complete section number as we iterate through the text.
+		 */
+		$prefixes = array();
+		
+		/*
+		 * Deal with each subsection, one at a time.
+		 */
+		$i=0;
+		foreach ($this->text as &$paragraph)
+		{
+			
+			/*
+			 * Detect the presence of a subsection prefix -- that is, a letter, number, or series of
+			 * letters that defines an individual subsection of text in a hierarchical fashion. The
+			 * subsection letter can be in one of five formats:
+			 * 
+			 * 		A. -> 1. -> a. -> (1) -> (a)
+			 *
+			 * Set aside the first five characters in this section of text. That's the maximum number
+			 * of characters that a prefix can occupy.
+			 */
+			$section_fragment = substr($paragraph, 0, 5);
+			
+			/*
+			 * Iterate through our PCRE candidates until we find one that matches (if, indeed, one
+			 * does at all).
+			 */
+			foreach ($prefix_candidates as $prefix)
+			{
+					
+				/*
+				 * If this prefix isn't found in this section fragment, then proceed to the next
+				 * prefix.
+				 */
+				preg_match($prefix, $section_fragment, $matches);
+				if (count($matches) == 0)
+				{
+					continue;
+				}
+				
+				/*
+				 * Great, we've successfully made a match -- we now know that this is the beginning
+				 * of a new numbered section. First, let's save a platonic ideal of this match.
+				 */
+				$match = trim($matches[0]);
+				
+				/*
+				 * Now we need to figure out what the entire section number is, only the very end of
+				 * which is our actual prefix. To start with, we need to modify our subsection
+				 * structure array to include our current prefix.
+				 * 
+				 * If this is our first time through, then this is easy -- our entire structure
+				 * consists of the current prefix.
+				 */
+				if (count($prefixes) == 0)
+				{
+					$prefixes[] = $match;
+				}
+				
+				/*
+				 * But if we already have a prefix stored in our array of prefixes for this section,
+				 * then we need to iterate through and see if there's a match.
+				 */
+				else
+				{
+					
+					/*
+					 * We must figure out where in the structure our current prefix lives. Iterate
+					 * through the prefix structure and look for anything that matches the regex
+					 * that matched our prefix.
+					 */
+					foreach ($prefixes as $key => &$prefix_component)
+					{
+						/*
+						 * We include a space after $prefix_component because this regex is looking
+						 * for a space after the prefix, something that would be there when finding
+						 * this match in the context of a section, but of course we've already
+						 * trimmed that out of $prefix_component.
+						 */
+						preg_match($prefix, $prefix_component.' ', $matches);
+						if (count($matches) == 0)
+						{
+							continue;
+						}
+						
+						/*
+						 * We've found a match! Update our array to reflect the current section
+						 * number, by modifying the relevant prefix component.
+						 */	
+						$prefix_component = $match;
+						
+						/*
+						* Also, set a flag so that we know that we made a match.
+						 */
+						$match_made = true;
+						
+						/*
+						 * If there are more elements in the array after this one, we need to zero
+						 * them out. That is, if we're in A4(c), and our last section was A4(b)6,
+						 * then we need to lop off that "6." So kill everything in the array after
+						 * this.
+						 */
+						if (count($prefixes) > $key)
+						{
+							$prefixes = array_slice($prefixes, 0, ($key+1));
+						}
+					}
+					
+					/*
+					 * If the $match_made flag hasn't been set, then we know that this is a new
+					 * prefix component, and we can append it to the prefix array.
+					 */
+					if (!isset($match_made))
+					{
+						$prefixes[] = $match;
+					}
+					else
+					{
+						unset($match_made);
+					}
+				}		
+				
+				/*
+				 * Iterate through the prefix structure and store each prefix section in our text
+				 * object. While we're at it, eliminate any periods.
+				 */
+				for ($j=0; $j<count($prefixes); $j++)
+				{
+					$output->$i->prefix_hierarchy->$j = str_replace('.', '', $prefixes[$j]);
+				}
+				
+				/*
+				 * And store the prefix list as a single string.
+				 */
+				$output->$i->prefix = implode('', $prefixes);
+		
+			}
+			
+			/*
+			 * Hack off the prefix at the beginning of the text and save what remains to $output.
+			 */
+			if (isset($output->$i->prefix))
+			{
+				$tmp2 = explode(' ', $paragraph);
+				unset($tmp2[0]);
+				$output->$i->text = implode(' ', $tmp2);
+			}
+			
+			/*
+			 * If no prefix was identified for this section, then it's a continuation of the prior
+			 * section (in reality, they're probably just paragraphs, not actually "sections").
+			 * Reuse the same section identifier and append the text as-is.
+			 */
+			if (!isset($output->$i->prefix) || empty($output->$i->prefix))
+			{
+				$output->$i->text = $paragraph;
+				$output->$i->prefix = $output->{$i-1}->prefix;
+				$output->$i->prefix_hierarchy = $output->{$i-1}->prefix_hierarchy;
+			}
+			
+			/*
+			 * We want to eliminate our matched prefix now, so that we don't mistakenly believe that
+			 * we've successfully made a match on our next loop through.
+			 */
+			unset($match);
+			
+			$i++;
+		}
+		
+		/*
+		 * Store the output within the class scope, give it a better name, free up some memory, and
+		 * report its success.
+		 */
+		$this->structured = $output;
+		unset($output);
+		return true;
+	}
+}
